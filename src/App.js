30bcc230d7298475e2076bdfdd949ddc5b2689d6
Pullareddy Tharun <pullareddytharun8@gmail.com>
2023-07-26 01:19:38 +0530

    Adding input and search bar

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ function App() {
       <div className="header bg-dark">
         <div className="row">
           <div className="brand my-1">
-            <h1>eStroe</h1>
+            <h1>eStore</h1>
+          </div>
+          <div className="inp-container p-0 m-4 h-25 w-50 bg-light">
+            <input className="form-control" type="text" placeholder="Enter" />
+            <button>
+              <i className="fa fa-search" />
+            </button>
           </div>
         </div>
       </div>
