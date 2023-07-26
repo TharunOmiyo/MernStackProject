c7fd9a0b326303ab65dc4cbf12c71dcc138e7d15
Pullareddy Tharun <pullareddytharun8@gmail.com>
2023-07-26 17:26:12 +0530

    Implemented TopNav

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
 import "./App.css";
+import TopNav from "./components/TopNav";
 
 function App() {
   return (
-    <div className="App bg-info">
-      <div className="header bg-dark">
-        <div className="row">
-          <div className="brand my-1">
-            <h1>eStore</h1>
-          </div>
-          <div className="inp-container p-0 m-4 h-25 w-50 bg-light">
-            <input className="form-control" type="text" placeholder="Enter" />
-            <button>
-              <i className="fa fa-search" />
-            </button>
-          </div>
-        </div>
-      </div>
+    <div className="App">
+      <TopNav />
     </div>
   );
 }
---
+++ src/components/TopNav/index.js
@@ -0,0 +1,50 @@
+import React from "react";
+import "./TopNav.css";
+
+export default function TopNav() {
+  return (
+    <div>
+      <div className="header bg-dark">
+        <div className="row bg-dark">
+          <div className="brand my-1 col-3">
+            <h1>eStore</h1>
+          </div>
+          <div className="inp-container p-0 m-4 h-25 bg-light col-6 ">
+            <div className="dropdown m-0 p-0">
+              <select className="select-btn m-0 p-0 w-100">
+                <option value="">Men</option>
+                <option value="">Women</option>
+                <option value="">Kids</option>
+              </select>
+            </div>
+
+            <input className="form-control" type="text" placeholder="Enter" />
+            <button>
+              <i className="fa fa-search" />
+            </button>
+          </div>
+          <div className="login-register p-0 col-2">
+            <i className="fa fa-user-circle user-icon" />
+            <h5>
+              <a>login</a>
+            </h5>
+            /
+            <h5>
+              <a>Register</a>
+            </h5>
+          </div>
+          <div className="cart-wishlist col-1 ">
+            <ul className="p-0">
+              <li className="list-icon">
+                <i className="fa fa-heart" />
+              </li>
+              <li className="list-icon">
+                <i className="fa fa-shopping-cart" />
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
